6724c3fa15dd5effad5dc7248db6fed31d878ba4
Roman Fomin <fomin.roman.andr@gmail.com>
2024-07-11 15:01:53 +0300

    init

---
+++ HC_Sandbox/Program.cs
@@ -0,0 +1,67 @@
+using GreenDonut;
+using HC_Sandbox;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+            .AddHttpContextAccessor()
+            .AddGraphQLServer()
+            .InitializeOnStartup()
+            .RegisterDbContext<EFDbContext>(DbContextKind.Pooled)
+            .AddQueryType()
+            .AddTypeExtension<TestQueries>()
+            //.AddMutationType()
+            //.AddTypeExtension<TestMutations>()
+            .AddGlobalObjectIdentification()
+            .AddType<PersonNode>()
+            .AddDataLoader<IPersonByIdDataLoader, PersonByIdDataLoader>()
+            .AddDataLoader<ICarsByPersonIdDataLoader, CarsByPersonIdDataLoader>();
+
+
+builder.Services.AddPooledDbContextFactory<EFDbContext>((cfg) =>
+{
+    cfg.UseLazyLoadingProxies();
+    cfg.UseSqlite($"Data Source=domain.db");
+    cfg.EnableSensitiveDataLogging(false);
+});
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<IDbContextFactory<EFDbContext>>().CreateDbContext();
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    EFDbContext.Populate(context);
+}
+
+app.UseCors(corsPolicyBuilder =>
+{
+    corsPolicyBuilder
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+
+    if (app.Environment.IsDevelopment())
+    {
+        corsPolicyBuilder
+        .AllowAnyOrigin();
+    }
+    else
+    {
+        corsPolicyBuilder
+        .WithOrigins(
+            "https://127.0.0.1",
+            "https://localhost",
+            "https://localhost:8000",
+
+            "http://127.0.0.1",
+            "http://localhost",
+            "http://localhost:8000")
+        .AllowCredentials();
+    }
+});
+
+app.MapGraphQL();
+app.Run();
---
+++ HC_Sandbox/Test.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HC_Sandbox;
+
+[Node]
+[ExtendObjectType<Person>]
+public class PersonNode
+{
+    [NodeResolver]
+    public static async Task<Person> GetAsync(
+        [ID] Guid id,
+        PersonByIdDataLoader personById,
+        CancellationToken cancellationToken
+    ) => await personById.LoadAsync(id, cancellationToken);
+
+    [DataLoader]
+    internal static async Task<IReadOnlyDictionary<Guid, Person>> GetPersonByIdAsync(
+        IReadOnlyList<Guid> ids,
+        [Service] IDbContextFactory<EFDbContext> dbContextFactory,
+        CancellationToken token)
+    {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync(token);
+        return await dbContext.Persons.Where(x => ids.Contains(x.Id)).ToDictionaryAsync(x => x.Id, EFDbContext.UnProxy, token);
+    }
+
+    [DataLoader]
+    internal static async Task<ILookup<Guid, Car>> GetCarsByPersonIdAsync(
+        IReadOnlyList<Guid> personIds,
+        [Service] IDbContextFactory<EFDbContext> dbContextFactory,
+        CancellationToken token)
+    {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync(token);
+        return dbContext.Cars.Where(x => personIds.Contains(x.PersonId)).ToLookup(x => x.PersonId);
+    }
+
+    public async Task<IEnumerable<Car>> GetCarAsync(
+        [Parent] Person person,
+        IPersonByIdDataLoader personById,
+        ICarsByPersonIdDataLoader carsByPersonId,
+        CancellationToken token)
+
+    {
+        var p = await personById.LoadAsync(person.Id, token);
+        var c = await carsByPersonId.LoadAsync(p.Id, token);
+        return c;
+    }
+}
+
+[QueryType]
+public class TestQueries
+{
+    // Just to get all ids
+    public async Task<IEnumerable<Person>> GetAllPersonsAsync(EFDbContext dbContext)
+    {
+        return dbContext.Persons;
+    }
+}
+
+
+//[MutationType]
+//public class TestMutations
+//{
+
+//}

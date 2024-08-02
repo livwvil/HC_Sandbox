69a18767e95478133f0b1f7686764b0fa3b75a68
Roman Fomin <fomin.roman.andr@gmail.com>
2024-08-02 11:50:33 +0300

    tmp

--- HC_Sandbox/Test.cs
+++ HC_Sandbox/Test.cs
@@ -1,62 +1,111 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace HC_Sandbox;
+﻿namespace Api;
 
 [Node]
-[ExtendObjectType<Person>]
-public class PersonNode
+public class Person(Guid id)
 {
-    [NodeResolver]
-    public static async Task<Person> GetAsync(
-        [ID] Guid id,
-        PersonByIdDataLoader personById,
-        CancellationToken cancellationToken
-    ) => await personById.LoadAsync(id, cancellationToken);
+    [ID]
+    public Guid Id { get; set; } = id;
+    public List<Guid> FriendIds { get; set; } = [];
+    public int Mood { get; set; } = 0;
+
+    public async Task<List<Person>> GetFriendsAsync(PersonByIdDataLoader dataLoader) => [.. (await dataLoader.LoadAsync(FriendIds))];
 
     [DataLoader]
-    internal static async Task<IReadOnlyDictionary<Guid, Person>> GetPersonByIdAsync(
-        IReadOnlyList<Guid> ids,
-        [Service] EFDbContext dbContext,
-        CancellationToken token)
-    {
-        return await dbContext.Persons.Where(x => ids.Contains(x.Id)).ToDictionaryAsync(x => x.Id, EFDbContext.UnProxy, token);
-    }
+    internal static async Task<IReadOnlyDictionary<Guid, Person>> GetPersonByIdAsync(IReadOnlyList<Guid> ids) =>
+        (await PersonRepo.Get(ids)).ToDictionary(x => x.Id);
+}
 
-    [DataLoader]
-    internal static async Task<ILookup<Guid, Car>> GetCarsByPersonIdAsync(
-        IReadOnlyList<Guid> personIds,
-        [Service] EFDbContext dbContext,
-        CancellationToken token)
-    {
-        return dbContext.Cars.Where(x => personIds.Contains(x.PersonId)).ToLookup(x => x.PersonId);
-    }
+public class PersonRepo
+{
+    private static Random _rnd = new();
 
-    public async Task<IEnumerable<Car>> GetCarAsync(
-        [Parent] Person person,
-        IPersonByIdDataLoader personById,
-        ICarsByPersonIdDataLoader carsByPersonId,
-        CancellationToken token)
-
+    public static async Task<List<Person>> Get(IReadOnlyList<Guid>? ids = null)
     {
-        var p = await personById.LoadAsync(person.Id, token);
-        var c = await carsByPersonId.LoadAsync(p.Id, token);
-        return c;
+        Console.WriteLine($"PersonRepo.Get({(ids is not null ? string.Join(", ", ids) : "")})");
+
+        // The database returns the same entities, but with a different mood field value, which changes depending on some external factors.
+        var p1 = new Person(new("00000000-0000-0000-0000-000000000001")) { Mood = _rnd.Next(0, 100) };
+        var p2 = new Person(new("00000000-0000-0000-0000-000000000002")) { Mood = _rnd.Next(0, 100) };
+        var p3 = new Person(new("00000000-0000-0000-0000-000000000003")) { Mood = _rnd.Next(0, 100) };
+        var p4 = new Person(new("00000000-0000-0000-0000-000000000004")) { Mood = _rnd.Next(0, 100) };
+        var p5 = new Person(new("00000000-0000-0000-0000-000000000005")) { Mood = _rnd.Next(0, 100) };
+        var p6 = new Person(new("00000000-0000-0000-0000-000000000006")) { Mood = _rnd.Next(0, 100) };
+
+        p1.FriendIds.Add(p2.Id);
+        p1.FriendIds.Add(p3.Id);
+        p1.FriendIds.Add(p5.Id);
+
+        p4.FriendIds.Add(p2.Id);
+        p4.FriendIds.Add(p3.Id);
+
+        p2.FriendIds.Add(p1.Id);
+        p2.FriendIds.Add(p4.Id);
+
+        p3.FriendIds.Add(p1.Id);
+        p3.FriendIds.Add(p4.Id);
+
+        p5.FriendIds.Add(p1.Id);
+        p5.FriendIds.Add(p6.Id);
+
+        p6.FriendIds.Add(p5.Id);
+
+        List<Person> repo = [p1, p2, p3, p4, p5, p6];
+
+
+        // Simulating a long database query
+        if (ids is not null)
+        {
+            var res = new List<Person>();
+            foreach (var id in ids)
+            {
+                await Task.Delay(_rnd.Next(500, 1000));
+                var it = repo.FirstOrDefault(x => x.Id == id);
+                if (it != null)
+                {
+                    res.Add(it);
+                }
+            }
+            return res;
+        }
+
+        return repo;
     }
 }
 
 [QueryType]
 public class TestQueries
 {
-    // Just to get all ids
-    public async Task<IEnumerable<Person>> GetAllPersonsAsync(EFDbContext dbContext)
+    public async Task<List<Person>> GetPersonsAsync() => await PersonRepo.Get();
+    public async Task<Person> GetPersonByIdWithDataLoaderAsync([ID] Guid id, PersonByIdDataLoader dataLoader) => await dataLoader.LoadAsync(id);
+    public async Task<IEnumerable<Person>> GetPersonByIdsWithDataLoaderAsync([ID] Guid[] id, PersonByIdDataLoader dataLoader) => await dataLoader.LoadAsync(id);
+    public async Task<Person> GetPersonByIdWithRepoAsync([ID] Guid id) => (await PersonRepo.Get([id])).FirstOrDefault()!;
+}
+
+
+[MutationType]
+public class TestMutations
+{
+    public async Task<Person> MutPersonByIdWithDataLoaderAsync([ID] Guid id, PersonByIdDataLoader dataLoader)
     {
-        return dbContext.Persons;
+        return await dataLoader.LoadAsync(id);
+    }
+
+    public async Task<Person> MutPersonByIdWithRepoAsync([ID] Guid id)
+    {
+        return (await PersonRepo.Get([id])).FirstOrDefault()!;
+    }
+
+
+
+    public async Task<IEnumerable<Person>> MutPersonByIdsWithDataLoaderAsync([ID] Guid[] ids, PersonByIdDataLoader dataLoader)
+    {
+        return await dataLoader.LoadAsync(ids);
+    }
+
+    public async Task<IEnumerable<Person>> MutPersonByIdsWithRepoAsync([ID] Guid[] ids)
+    {
+        return await PersonRepo.Get(ids);
     }
 }
 
 
-//[MutationType]
-//public class TestMutations
-//{
-
-//}
